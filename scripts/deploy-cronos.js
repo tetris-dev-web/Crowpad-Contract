14a415a7c6bef9788e8a13ed946fb4b08de4cc93
tetris.dev.web <tetris.dev.web@gmail.com>
2022-01-27 19:48:49 +0800

    deploy script

--- scripts/deploy-cronos.js
+++ scripts/deploy-cronos.js
@@ -3,6 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+
 const hre = require("hardhat");
 
 async function main() {
